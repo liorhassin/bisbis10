e9400b4ca7e3acaaf9868976bc6bec321976b3ec
Lior Hassin <liorhassin3@gmail.com>
2024-05-06 13:53:37 +0300

    added repository query for get restaurants by cuisine functionality

--- src/main/java/com/att/tdp/bisbis10/repositories/RestaurantRepository.java
+++ src/main/java/com/att/tdp/bisbis10/repositories/RestaurantRepository.java
@@ -2,6 +2,14 @@ package com.att.tdp.bisbis10.repositories;
 
 import com.att.tdp.bisbis10.entities.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query("SELECT r FROM Restaurant r JOIN r.cuisines c WHERE c ilike :cuisine")
+    List<Restaurant> getRestaurantsByCuisine(@Param("cuisine") String cuisine);
+
 }
