4ecc484ade21abe470fe5f74189e3fb925fa7b30
Lior Hassin <liorhassin3@gmail.com>
2024-05-06 13:37:35 +0300

    controller functionality, without service support

--- src/main/java/com/att/tdp/bisbis10/controller/RestaurantController.java
+++ src/main/java/com/att/tdp/bisbis10/controller/RestaurantController.java
@@ -5,7 +5,10 @@ import com.att.tdp.bisbis10.entities.Restaurant;
 import com.att.tdp.bisbis10.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurants")
@@ -21,9 +24,38 @@ public class RestaurantController {
                 .orElse(ResponseEntity.noContent().build());
     }
 
+    @GetMapping()
+    ResponseEntity<List<Restaurant>> getAllRestaurants(@RequestParam(name = "cuisine", required = false) String cuisine){
+        ResponseEntity<List<Restaurant>> responseEntity;
+        if(cuisine!=null && !cuisine.isEmpty()){
+            responseEntity = ResponseEntity.ok(restaurantService.findRestaurantsByCuisine(cuisine));
+        }else{
+            responseEntity = ResponseEntity.ok(restaurantService.getAllRestaurants());
+        }
+        return responseEntity;
+    }
+
     @PostMapping
     ResponseEntity<?> newRestaurant(@RequestBody RestaurantDTO restaurant){
         restaurantService.newRestaurant(restaurant);
         return ResponseEntity.status(201).body(null);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<?> editRestaurant(@RequestBody Restaurant newRestaurant, @PathVariable Long id, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+        return ResponseEntity.ok(restaurantService.editRestaurant(newRestaurant, id));
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> deleteRestaurant(@PathVariable Long id){
+        try {
+            restaurantService.deleteRestaurant(id);
+            return ResponseEntity.status(204).body(null);
+        }catch(Exception e){
+            return ResponseEntity.status(500).body(null);
+        }
     }
 }
