28e74b1c9fff55cf666c172a5d126e7b07e5156c
Lior Hassin <liorhassin3@gmail.com>
2024-05-08 15:41:00 +0300

    add dish support service and controller

--- src/main/java/com/att/tdp/bisbis10/controller/DishController.java
+++ src/main/java/com/att/tdp/bisbis10/controller/DishController.java
@@ -1,8 +1,11 @@
 package com.att.tdp.bisbis10.controller;
 
+import com.att.tdp.bisbis10.dto.DishDTO;
+import com.att.tdp.bisbis10.entities.Dish;
+import com.att.tdp.bisbis10.services.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/restaurants/{id}/dishes")
@@ -10,5 +13,12 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+
+    @PostMapping
+    ResponseEntity<?> addDish(@PathVariable Long id, @RequestBody DishDTO dishDTO){
+        Dish dish = dishService.addDish(dishDTO, id);
+        if(dish == null) return ResponseEntity.status(500).body(null);
+        return ResponseEntity.status(201).body(null);
+    }
 
 }
--- src/main/java/com/att/tdp/bisbis10/services/DishService.java
+++ src/main/java/com/att/tdp/bisbis10/services/DishService.java
@@ -1,13 +1,40 @@
 package com.att.tdp.bisbis10.services;
 
+import com.att.tdp.bisbis10.dto.DishDTO;
+import com.att.tdp.bisbis10.entities.Dish;
+import com.att.tdp.bisbis10.entities.Restaurant;
 import com.att.tdp.bisbis10.repositories.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DishService {
 
     @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
     private DishRepository dishRepository;
 
+    public Dish addDish(DishDTO dto, Long restaurantId){
+        Dish dish = dishDtoToEntity(dto, restaurantId);
+        return dishRepository.save(dish);
+    }
+
+    private Dish dishDtoToEntity(DishDTO dto, Long restaurantId){
+        Dish dish = new Dish();
+        dish.setPrice(dto.price());
+        dish.setName(dto.name());
+        dish.setDescription(dto.description());
+
+        //TODO - Check how orElse works instead of this code:
+        Optional<Restaurant> restaurantOptional = restaurantService.getRestaurantById(restaurantId);
+        if(restaurantOptional.isPresent())
+            dish.setRestaurant(restaurantOptional.get());
+        else dish.setRestaurant(null);
+
+        return dish;
+    }
 }
