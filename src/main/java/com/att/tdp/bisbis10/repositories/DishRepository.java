da7039145f86cd306fe67b38d9d34ea71d1bba82
Lior Hassin <liorhassin3@gmail.com>
2024-05-08 15:37:35 +0300

    dish entity with restaurant column connection

--- src/main/java/com/att/tdp/bisbis10/repositories/DishRepository.java
+++ src/main/java/com/att/tdp/bisbis10/repositories/DishRepository.java
@@ -1,6 +1,8 @@
 package com.att.tdp.bisbis10.repositories;
 
+import com.att.tdp.bisbis10.entities.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DishRepository extends JpaRepository<Dish, Long> {
+
 }
